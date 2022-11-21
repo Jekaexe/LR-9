c6bb27a171919402f7719c4de30969867422a10f
Jekaexe <117809005+Jekaexe@users.noreply.github.com>
2022-11-21 17:15:21 +0300

    Update 5.pas

--- 5.pas
+++ 5.pas
@@ -1,18 +1,55 @@
+program lab_9_zadanie_5;
+
 const
   n = 5;
 
 var
   a: array[1..n] of integer;
   b: array[1..n] of integer;
-  index_a, index_b, max_a, max_b, max_index_a, max_index_b, min_a, min_b, min_index_a, min_index_b, i: integer;
-
+  index_a, index_b, max_a, max_b, max_index_a, max_index_b, min_a, min_b, min_index_a, min_index_b, i,k: integer;
+procedure max_array_a();
+var i:integer;
 begin
-  index_a := 0;
-  index_b := 0;
-  max_index_a := 0;
-  max_a := 0;
-  max_index_b := 0;
-  max_b := 0;
+    for i := 1 to n do
+    if a[i] > max_a then
+    begin
+      max_a := a[i];
+      max_index_a := i;
+    end;
+end;
+procedure max_array_b();
+var i:integer;
+begin
+   for i := 1 to n do
+    if b[i] > max_b then
+    begin
+      max_b := b[i];
+      max_index_b := i;
+    end;
+end;
+procedure min_array_a();
+begin
+  var i:integer;
+  min_a := max_a;
+  for i := 1 to n do
+    if a[i] < min_a then
+    begin
+      min_a := a[i];
+      min_index_a := i;
+    end;
+end;
+procedure min_array_b();
+  var i:integer;
+  begin
+   min_b := max_b;
+   for i := 1 to n do
+    if b[i] < min_b then
+    begin
+      min_b := b[i];
+      min_index_b := i;
+    end; 
+  end;
+begin
   for i := 1 to n do
   begin
     write('a[', i, '] = ');
@@ -25,32 +62,10 @@ begin
   end;
   write('Массив a: ', a);
   writeln(' Массив b: ', b);
-  for i := 1 to n do
-    if a[i] > max_a then
-    begin
-      max_a := a[i];
-      max_index_a := i;
-    end;
-  min_a := max_a;
-  for i := 1 to n do
-    if a[i] < min_a then
-    begin
-      min_a := a[i];
-      min_index_a := i;
-    end;
-  for i := 1 to n do
-    if b[i] > max_b then
-    begin
-      max_b := b[i];
-      max_index_b := i;
-    end;
-  min_b := max_b;
-  for i := 1 to n do
-    if b[i] < min_b then
-    begin
-      min_b := b[i];
-      min_index_b := i;
-    end;
+  max_array_a();
+  min_array_a();
+  max_array_b();
+  min_array_b();
   for i := 1 to n do
     if (a[i] mod 5 = 0) then
       index_a := i;
